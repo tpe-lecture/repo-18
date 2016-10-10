515e276b582e6878dfbb67bb85dac781a4df88a3
stella <stella@stellas-mbp>
2016-10-10 20:12:16 +0200

    Tests für das Potenzieren von Komplexen Zahlen hinzugefügt. Methode für die Berechnung der K.Zahlen verändert .

--- 02_junit_javadoc/02_unit_test/src/ComplexNumberTest.java
+++ 02_junit_javadoc/02_unit_test/src/ComplexNumberTest.java
@@ -12,17 +12,17 @@ public class ComplexNumberTest {
     @Test
     public void testAddition() {
         // Beispiel für einen Test der Addition
-        
+
         // Durchzuführende Tests:
         // (4 + -5i) + (2 + 11i) = (6 + 6i)
-        // (4 + -5i) + 3         = (7 + -5i)        
+        // (4 + -5i) + 3         = (7 + -5i)
         ComplexNumber a = new ComplexNumber(4, -5);
         ComplexNumber b = new ComplexNumber(2, 11);
 
         assertEquals(new ComplexNumber(6, 6), a.add(b));
         assertEquals(new ComplexNumber(7, -5), a.add(3));
     }
-    
+
     /**
      * Testet die Subtraktion.
      */
@@ -35,7 +35,7 @@ public class ComplexNumberTest {
         // TODO: Test schreiben
         ComplexNumber a = new ComplexNumber(4, -5);
         ComplexNumber b = new ComplexNumber(2, 11);
-        
+
         assertEquals(new ComplexNumber(2, -16), a.subtract(b));
         assertEquals(new ComplexNumber( -2, 16), b.subtract(a));
     }
@@ -49,13 +49,13 @@ public class ComplexNumberTest {
         // (2 + -4i) * (-3 + 5i) = (14  +  22i)
         // (2 +  5i) * ( 3 + 7i) = (-29 +  29i)
         // (2 + -4i) * 4         = (  8 + -16i)
-        
+
         // TODO: Test schreiben
         ComplexNumber a = new ComplexNumber(2, -4);
         ComplexNumber b = new ComplexNumber(-3, 5);
         ComplexNumber c = new ComplexNumber(2, 5);
         ComplexNumber d = new ComplexNumber(3, 7);
-        
+
         assertEquals(new ComplexNumber(14, 22), a.multiply(b));
         assertEquals(new ComplexNumber(-29, 29), c.multiply(d));
         assertEquals(new ComplexNumber(8, -16), a.multiply(4));
@@ -70,21 +70,21 @@ public class ComplexNumberTest {
         // (4 + -8i) : (3 + 4i) = (-0,8 + -1,6i)
         // (1 +  0i) : (0 + 1i) = (   0 +   -1i)
         // (8 + 16i) : 4        = (   2 +    4i)
-        
+
         // TODO: Test schreiben
         ComplexNumber a = new ComplexNumber(4, -8);
         ComplexNumber b = new ComplexNumber(3, 4);
         ComplexNumber c = new ComplexNumber(1, 0);
         ComplexNumber d = new ComplexNumber(0, 1);
         ComplexNumber e = new ComplexNumber(8, 16);
-        
+
         assertEquals(new ComplexNumber(-0.8, -1.6), a.divide(b));
         assertEquals(new ComplexNumber(0, -1), c.divide(d));
         assertEquals(new ComplexNumber(2, 4), e.divide(4));
     }
-        
-    
-    
+
+
+
 
     /**
      * Testet die Erzeugung der konjugiert Komplexen Zahl.
@@ -94,13 +94,11 @@ public class ComplexNumberTest {
         // Durchzuführende Tests:
         // (3   +  5i) --conj--> (3   + -5i)
         // (-2  + -4i) --conj--> (-2  + 4i)
-          
+
         // TODO: Test schreiben
         ComplexNumber a = new ComplexNumber(3, 5);
         ComplexNumber b = new ComplexNumber(-2, -4);
-        
-        
-        
+
         assertEquals(new ComplexNumber( 3, -5), a.conjugate());
         assertEquals(new ComplexNumber( -2, 4), b.conjugate());
     }
@@ -116,10 +114,12 @@ public class ComplexNumberTest {
 
         // TODO: Test schreiben
         ComplexNumber a = new ComplexNumber(3,-4);
-        
+        ComplexNumber b = new ComplexNumber(0, 3);
+
         assertEquals(5.0, a.abs(), 0.1);
-    }   
-    
+        assertEquals(3.0, b.abs(), 0.1);
+    }
+
     /**
      * Testet das Potenzieren.
      */
@@ -132,6 +132,12 @@ public class ComplexNumberTest {
         // (2 + -4i)^6 = (7488 + -2816i)
 
         // TODO: Test schreiben
+        ComplexNumber a = new ComplexNumber(2, -4);
+
+        assertEquals(new ComplexNumber(2,-4), a.power(1));
+        assertEquals(new ComplexNumber(-12, -16), a.power(2));
+        assertEquals(new ComplexNumber(-88, 16), a.power(3));
+        assertEquals(new ComplexNumber(7488, -2816), a.power(6));
     }
 }
 
