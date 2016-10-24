fcd5f277fc82b6880744eb5e73b16a2e76afdae7
stella <stella@10.10.114.17>
2016-10-24 15:05:41 +0200

    Anwendung von Polymorphie, um alle Tiere mit nur einer einzigen Methode zu füttern Array tiere wurde angelegt

--- 04_objektorientierung/05_polymorphie/src/tpe/oo/polymorphie/Futterstelle.java
+++ 04_objektorientierung/05_polymorphie/src/tpe/oo/polymorphie/Futterstelle.java
@@ -1,38 +1,20 @@
 package tpe.oo.polymorphie;
 
-import tpe.oo.polymorphie.tiere.Affe;
-import tpe.oo.polymorphie.tiere.Giraffe;
-import tpe.oo.polymorphie.tiere.Gorilla;
+import tpe.oo.polymorphie.tiere.ZooTier;
 
 /**
  * Fütterung der Tiere.
  */
 public class Futterstelle {
 
+
     /**
-     * Füttert den Affen.
-     * 
-     * @param affe Affe, der gefüttert werden soll. 
+     *  Füttert alle Tiere.
+     *
+     * @param zootier ZooTiere, die gefüttert werden sollen
      */
-    public void gibFutter(Affe affe) {
-        affe.fuettern();
-    }
-    
-    /**
-     * Füttert den Gorilla.
-     * 
-     * @param gorilla Gorilla, der gefüttert werden soll. 
-     */
-    public void gibFutter(Gorilla gorilla) {
-        gorilla.fuettern();
-    }
-    
-    /**
-     * Füttert die Giraffe.
-     * 
-     * @param giraffe Giraffe, die gefüttert werden soll. 
-     */
-    public void gibFutter(Giraffe giraffe) {
-        giraffe.fuettern();
+    public void gibFutter(ZooTier zootier) {
+        zootier.fuettern();
+
     }
 }
