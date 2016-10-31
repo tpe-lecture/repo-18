898d126b940d176a33dab3a7f27efa1e2d79831b
stella <stella@10.10.126.15>
2016-10-31 11:59:27 +0100

    Üben von abstracten Klassen:
    -einfügen einer abstracten Klasse Figur
    -vererbung von dieser Figur zu subklassen
    -einfügen von gemeinsamkeit der Figuren in Figuren Klasse

---
+++ 04_objektorientierung/07_abstrakte_klassen/src/tpe/oo/abstrakt/Figur.java
@@ -0,0 +1,19 @@
+package tpe.oo.abstrakt;
+
+/**
+ * Klasse die Figuren verwaltet.
+ *
+ * @author stella
+ *
+ */
+public abstract class Figur {
+
+
+    /**
+     * Berechnet die Fläche.
+     *
+     * @return die Fläche.
+     */
+    public abstract double flaeche();
+
+}
