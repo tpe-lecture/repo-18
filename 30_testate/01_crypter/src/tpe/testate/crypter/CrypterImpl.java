7be56ef85a61b61d9bb0dc44a284b2ce598cedfb
stella <stella@stellas-mbp>
2016-12-02 19:16:55 +0100

    Testat Änderungen
    -Javadoc ergänzt
    -ungültige Zeichen werden jetzt ignoriert

--- 30_testate/01_crypter/src/tpe/testate/crypter/CrypterImpl.java
+++ 30_testate/01_crypter/src/tpe/testate/crypter/CrypterImpl.java
@@ -1,24 +1,51 @@
 package tpe.testate.crypter;
 
+/**
+ * Die Klasse implemetiert das Interface Crypter und ist für die Verschlüsselung
+ * und Entschlüsselung von Texten zuständig
+ *
+ * @author Stella Neser
+ *
+ */
 public class CrypterImpl implements Crypter {
-
+	/**
+	 * Verschlüsselt den gegebenen Text. Enthält die Eingabe Großbuchstaben, so
+	 * werden diese in Kleinbuchstaben umgewandelt. Ungültige Zeichen werden
+	 * ignoriert und nicht in den verschlüsselten Text übernommen.
+	 *
+	 * @param input
+	 *            Text, der verschlüsselt werden soll.
+	 * @return Verschlüsselter Text.
+	 */
 	@Override
 	public String encrypt(String input) {
 		String encrypted = "";
 		input = input.toLowerCase(); // nur kleinbuchstaben erlaubt
 		for (int i = 0; i < input.length(); i++) {
-			encrypted += encode(input.charAt(i));
+			if (isValid(input.charAt(i))) {
+				encrypted += encode(input.charAt(i));
+			}
 		}
 		return encrypted;
 	}
 
+	/**
+	 * Entschlüsselt den gegebenen Text. Enthält die Eingabe Großbuchstaben,
+	 * oder andere ungültige Zeichen, so wirft die Methode eine Ausnahme.
+	 *
+	 * @param input
+	 *            Text, der entschlüsselt werden soll.
+	 * @return Entschlüsselter Text.
+	 * @throws IllegalArgumentException
+	 *             Wird geworfen, wenn die Eingabe ungültige Zeichen
+	 *             (Großbuchstaben) enthält.
+	 */
 	@Override
 	public String decrypt(String input) throws IllegalArgumentException {
 		String decrypted = "";
 		for (int i = 0; i < input.length(); i++) {
 
-			if ((input.charAt(i) >= 'a' && input.charAt(i) <= 'z') || (input.charAt(i) >= '0' && input.charAt(i) <= '9')
-					|| input.charAt(i) == ' ') {
+			if (isValid(input.charAt(i))) {
 				decrypted += encode(input.charAt(i));
 			} else {
 				throw new IllegalArgumentException();
@@ -27,6 +54,22 @@ public class CrypterImpl implements Crypter {
 		return decrypted;
 	}
 
+	/**
+	 * Prüft Zeichen auf Gültigkeit
+	 *
+	 * @param input das zu untersuchende Zeichen
+	 * @return True wenn gültiges zeichen, False wenn nicht
+	 */
+	private boolean isValid(char input) {
+		return ((input >= 'a' && input <= 'z') || (input >= '0' && input <= '9') || input == ' ');
+	}
+
+	/**
+	 * Bestimmte zeichen werden hier durch andere erstezt (verschlüsselt)
+	 *
+	 * @param input das zu verschlüsselnde Zeichen
+	 * @return gibt die Verschlüsselung der bestimmten Zeichen zurueck
+	 */
 	private char encode(char input) {
 		switch (input) {
 		case 'e':
